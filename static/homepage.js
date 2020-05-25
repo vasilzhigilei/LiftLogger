c48922ef7b2a26d96ce8feb9a6188078a7b20028
Vasil Zhigilei <vasil.zhigilei@gmail.com>
2020-05-24 22:09:42 -0400

    input limits and validation

--- static/homepage.js
+++ static/homepage.js
@@ -2,17 +2,20 @@
 var form = document.getElementById("analyzeform")
 form.addEventListener('submit', submitForm);
 
-
-
 function submitForm(event){
     // ugh this code is UGLY! Will have to DEFINITELY redo all of this!
     event.preventDefault()
-    maximums = getMaximums()
+    data = getData()
+    if(data.length < 0)
+        return // if no data, don't do anything
+    for(datum in data){
 
+    }
 
 }
 
 function getData() {
+    // returns array of sets of lifts and weights/reps
     inputblocks = document.querySelectorAll(".inputblock")
     data = []
     for(var i = 0; i < inputblocks.length; i++){
@@ -21,12 +24,11 @@ function getData() {
             data.append([inputblocks[i].id, result[0], result[1]])
         }
     }
-
-
-    return {dl_max, s_max, bp_max, ohp_max}
+    return data
 }
 
 function getFields(inputblock){
+    // helper function for getData
     fields = inputblock.getElementsByClassName("form-control")
     result = []
     for(field in fields){
@@ -38,3 +40,33 @@ function getFields(inputblock){
     }
     return result
 }
+
+// Restricts input for the given textbox to the given inputFilter.
+function setInputFilter(textbox, inputFilter) {
+    ["input", "keydown", "keyup", "mousedown", "mouseup", "select", "contextmenu", "drop"].forEach(function(event) {
+        textbox.addEventListener(event, function() {
+            if (inputFilter(this.value)) {
+                this.oldValue = this.value;
+                this.oldSelectionStart = this.selectionStart;
+                this.oldSelectionEnd = this.selectionEnd;
+            } else if (this.hasOwnProperty("oldValue")) {
+                this.value = this.oldValue;
+                this.setSelectionRange(this.oldSelectionStart, this.oldSelectionEnd);
+            } else {
+                this.value = "";
+            }
+        });
+    });
+}
+
+idsInt = ["dl-weight", "dl-reps", "s-weight", "s-reps", "bp-weight", "bp-reps", "ohp-weight", "ohp-reps", "age"]
+
+// limit lift weights/reps from 0 to 1200
+for(id in idsInt) {
+    setInputFilter(document.getElementById(id), function (value) {
+        return /^\d*$/.test(value) && (value === "" || parseInt(value) <= 1200);
+    });
+}
+// limit input of person weight to two decimal places and no more than 1000 (big upper limit :))
+setInputFilter(document.getElementById("currencyTextBox"), function(value) {
+    return /^-?\d*[.,]?\d{0,2}$/.test(value) && (value === "" || parseFloat(value) <= 1000); });
