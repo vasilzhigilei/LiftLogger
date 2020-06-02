906ca56272a71d915bb3aeeef515d41eff49ce00
Vasil Zhigilei <vasil.zhigilei@gmail.com>
2020-06-02 15:31:51 -0400

    IT FINALLY WORKS

--- dbmanager.go
+++ dbmanager.go
@@ -38,12 +38,19 @@ func (d *Database) LogLifts(user *User) error {
 	execstring := `
 UPDATE userdata
 SET age = $1,
-weight = CASE WHEN date[array_upper(date,1)] = $7 THEN weight[array_upper(weight, 1)] = $2 ELSE weight = array_append(weight, $2) END,
-deadlift = CASE WHEN date[array_upper(date,1)] = $7 THEN deadlift[array_upper(deadlift, 1)] = $2 ELSE deadlift = array_append(deadlift, $3) END,
-squat = CASE WHEN date[array_upper(date,1)] = $7 THEN squat[array_upper(squat, 1)] = $2 ELSE squat = array_append(squat, $4) END,
-bench = CASE WHEN date[array_upper(date,1)] = $7 THEN bench[array_upper(bench, 1)] = $2 ELSE bench = array_append(bench, $5) END,
-overhead = CASE WHEN date[array_upper(date,1)] = $7 THEN overhead[array_upper(overhead, 1)] = $2 ELSE overhead = array_append(overhead, $6) END,
-WHERE email = $8`
+weight = CASE WHEN date[array_upper(date,1)] = $7 THEN array_replace(weight, weight[array_upper(weight, 1)], $2) 
+ELSE array_append(weight, $2) END,
+deadlift = CASE WHEN date[array_upper(date,1)] = $7 THEN array_replace(deadlift, deadlift[array_upper(deadlift, 1)], $3) 
+ELSE array_append(deadlift, $3) END,
+squat = CASE WHEN date[array_upper(date,1)] = $7 THEN array_replace(squat, squat[array_upper(squat, 1)], $4) 
+ELSE array_append(squat, $4) END,
+bench = CASE WHEN date[array_upper(date,1)] = $7 THEN array_replace(bench, bench[array_upper(bench, 1)], $5) 
+ELSE array_append(bench, $5) END,
+overhead = CASE WHEN date[array_upper(date,1)] = $7 THEN array_replace(overhead, overhead[array_upper(overhead, 1)], $6) 
+ELSE array_append(overhead, $6) END,
+date = CASE WHEN date[array_upper(date,1)] = $7 THEN array_replace(date, date[array_upper(date, 1)], $7) 
+ELSE array_append(date, $7) END
+WHERE email = $8;`
 	_, err := d.conn.Exec(context.Background(), execstring, user.Age, user.Weight, user.Deadlift, user.Squat,
 		user.Bench, user.Overhead, fmt.Sprint(time.Now().Date()), user.Email)
 	return err
