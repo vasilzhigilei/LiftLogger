f7c54f68d9c9377601092ec8a7d23801c4b05c00
Vasil Zhigilei <vasil.zhigilei@gmail.com>
2020-06-03 20:39:21 -0400

    chart works great :)

--- static/chartscript.js
+++ static/chartscript.js
@@ -5,32 +5,50 @@ $.ajax({
     url: "/getlifts" || window.location.pathname,
     type: "POST",
     success: function (data) {
-        console.log("lifts fetched for chart")
-        console.log(data)
+        jsondata = JSON.parse(data)
         var chart = new Chart(ctx, {
             // The type of chart we want to create
             type: 'line',
 
             // The data for our dataset
             data: {
-                labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+                labels: jsondata["Date"],
                 datasets: [{
-                    label: 'Weight',
-                    backgroundColor: 'rgba(0, 0, 0, 0)',
-                    borderColor: 'rgb(255, 99, 132)',
-                    data: [0, 10, 5, 2, 20, 30, 45]
-                },
+                        label: 'Weight',
+                        backgroundColor: 'rgba(0, 0, 0, 0)',
+                        borderColor: 'rgb(99,255,107)',
+                        data: jsondata["Weight"]
+                    },
                     {
                         label: 'Deadlift',
                         backgroundColor: 'rgba(0, 0, 0, 0)',
+                        borderColor: 'rgb(221,99,255)',
+                        data: jsondata["Deadlift"]
+                    },
+                    {
+                        label: 'Squat',
+                        backgroundColor: 'rgba(0, 0, 0, 0)',
+                        borderColor: 'rgb(255,216,99)',
+                        data: jsondata["Squat"]
+                    },
+                    {
+                        label: 'Deadlift',
+                        backgroundColor: 'rgba(0, 0, 0, 0)',
+                        borderColor: 'rgb(104,99,255)',
+                        data: jsondata["Bench"]
+                    },
+                    {
+                        label: 'Overhead',
+                        backgroundColor: 'rgba(0, 0, 0, 0)',
                         borderColor: 'rgb(99, 206, 255)',
-                        data: [45, 5, 10, 2, 30, 20, 10]
+                        data: jsondata["Overhead"]
                     }]
             },
 
             // Configuration options go here
             options: {}
         });
+        console.log("lifts fetched for chart")
     },
     error: function (jXHR, textStatus, errorThrown) {
         alert(errorThrown);
